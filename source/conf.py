0c971bac7c91f794fe62ff99eb64c919be29dfbd
James Taylor <James.Taylor@studica.com>
2022-11-01 14:56:04 -0400

    fix

--- source/conf.py
+++ source/conf.py
@@ -45,7 +45,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = ["docs/Basic-Bot/Chassis/index.rst", "docs/Basic-Bot/OMS/index.rst", "docs/Basic-Bot/Programming/index.rst"]
 
 master_doc = "index"
 
